9ee1972631dc008cab17e82986fc1d1ef4898372
Christoph Zöttl <christoph@mangomint.com>
2012-11-23 23:09:18 +0100

    Read mails from Raven instead of reading from smtp server each request

--- MailSendbox/Controllers/InboxController.cs
+++ MailSendbox/Controllers/InboxController.cs
@@ -1,24 +1,27 @@
-﻿using MailSendbox.Models.Repositories;
+﻿using MailSendbox.Core;
+using MailSendbox.Models.Repositories;
 using MailSendbox.ViewModels.Home;
 using System.Linq;
 using System.Web.Mvc;
+using Raven.Client;
 
 namespace MailSendbox.Controllers
 {
     public class InboxController : RavenController
     {
-        private readonly IMailRepository _mailRepo;
-
-        public InboxController(IMailRepository mailRepo)
-        {
-            _mailRepo = mailRepo;
-        }
-
         public ActionResult Index()
         {
             var viewModel = new IndexViewModel()
             {
-                Mails = _mailRepo.Get().Take(10)
+                Mails = Session.Query<Mail>().Take(10).Select(x => new Models.Mail()
+                    {
+                        Body = x.Body,
+                        Date = x.ReceivedDate,
+                        From = x.From,
+                        MessageId = x.MessageId,
+                        Subject = x.Subject,
+                        To = x.To,
+                    })
             };
             return View(viewModel);
         }
--- MailSendbox/Controllers/RavenController.cs
+++ MailSendbox/Controllers/RavenController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MailSendbox.Infrastructure.Raven;
 using Raven.Client;
 
 namespace MailSendbox.Controllers
 {
+    [RavenActionFilter]
     public abstract class RavenController : Controller
     {
         public new IDocumentSession Session { get; set; }
